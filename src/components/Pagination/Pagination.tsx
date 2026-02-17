5ba1ec213ce71d747b9ab0059f4dddef8696a3f1
Nazar <nazar.shevchuk.121@gmail.com>
2026-02-17 22:14:54 +0100

    sol1

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,75 @@
-export const Pagination = () => {};
+// import { useState } from 'react';
+import { getNumbers } from '../../utils';
+
+interface PaginationType {
+  total: number;
+  perPage: number;
+  currentPage: number;
+  onPageChange: (
+    value: number,
+  ) => React.MouseEventHandler<HTMLAnchorElement> | undefined | void;
+}
+
+export const Pagination = ({
+  total,
+  perPage,
+  currentPage,
+  onPageChange,
+}: PaginationType) => {
+  const lastPageNumber = Math.ceil(total / perPage);
+  const countOfPages = getNumbers(1, Math.ceil(total / perPage));
+
+  return (
+    <>
+      <ul className="pagination">
+        <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+          <a
+            data-cy="prevLink"
+            className="page-link"
+            href="#prev"
+            aria-disabled={currentPage === 1 ? 'true' : 'false'}
+            onClick={
+              currentPage !== 1 ? () => onPageChange(currentPage - 1) : () => {}
+            }
+          >
+            «
+          </a>
+        </li>
+
+        {countOfPages.map(item => (
+          <li
+            className={`page-item ${item === currentPage ? 'active' : ''}`}
+            key={item}
+          >
+            <a
+              data-cy="pageLink"
+              className="page-link"
+              href="#1"
+              onClick={() => onPageChange(item)}
+            >
+              {item}
+            </a>
+          </li>
+        ))}
+
+        <li
+          className={`page-item ${currentPage === lastPageNumber ? 'disabled' : ''}`}
+        >
+          <a
+            data-cy="nextLink"
+            className="page-link"
+            href="#next"
+            aria-disabled={currentPage === lastPageNumber ? 'true' : 'false'}
+            onClick={
+              currentPage !== lastPageNumber
+                ? () => onPageChange(currentPage + 1)
+                : () => {}
+            }
+          >
+            »
+          </a>
+        </li>
+      </ul>
+    </>
+  );
+};
